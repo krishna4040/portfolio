bdd34b5d312731634b47e8034b56e08703da71f1
krishna jain <krishnajain5050@gmail.com>
2025-07-02 03:03:25 +0530

    style(AdminLogin): navbar height subtracted

--- client/src/components/admin/AdminLogin.jsx
+++ client/src/components/admin/AdminLogin.jsx
@@ -36,7 +36,7 @@ const AdminLogin = () => {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50 px-4 py-12 transition-colors duration-300 sm:px-6 lg:px-8 dark:bg-gray-900">
+    <div className="flex min-h-[calc(100vh-128px)] items-center justify-center bg-gray-50 px-4 py-12 transition-colors duration-300 sm:px-6 lg:px-8 dark:bg-gray-900">
       <div className="w-full max-w-md space-y-8 rounded-lg bg-white p-8 shadow-lg transition-colors duration-300 dark:bg-gray-700">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
