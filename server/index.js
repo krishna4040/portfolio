35d1c24f5904ca128ae3b23ee7d6074f384d44fb
krishna jain <krishnajain5050@gmail.com>
2025-07-02 01:17:02 +0530

    refactor(server): sendmail removed

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url"
 import { dbConnect } from "./config/dbConnect.js"
-import Message from "./models/message.js"
-// import { sendMail } from "./config/nodemailer.js"
 
 // Import routes
 import authRoutes from "./routes/auth.js"
@@ -55,48 +53,4 @@ app.listen(port, () => {
   console.log(
     `Environment: ${env === "production" ? "production" : "development"}`,
   )
-})
-
-// Contact form endpoint
-app.post("/api/createEntry", async (req, res) => {
-  try {
-    const { name, email, subject, message } = req.body
-    if (!name || !email || !subject || !message) {
-      return res.status(400).json({
-        success: false,
-        message: "All fields are required",
-      })
-    }
-
-    await Message.create({
-      name,
-      email,
-      subject,
-      message,
-    })
-
-    res.status(200).json({
-      success: true,
-      message: "Message sent successfully",
-    })
-  } catch (error) {
-    console.error("Error creating message:", error)
-    res.status(500).json({
-      success: false,
-      message: "Failed to send message",
-    })
-  }
-})
-
-// Resume download endpoint (optional)
-app.get("/api/download", (_, res) => {
-  const fileURL = "./utils/resume.pdf"
-  const fileName = "resume.pdf"
-
-  res.download(fileURL, fileName, (err) => {
-    if (err) {
-      console.error(err)
-      res.status(404).send("File not found")
-    }
-  })
 })
